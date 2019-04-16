ea362cdf7e04988b59b40e62fd4f0daec6d2062d
yueyueyue <643947519@qq.com>
2019-04-16 18:21:14 +0800

    调试好的模板

--- src/main/java/com/wofeng/articlemanagement/mvcConfig/WebMvcConfig.java
+++ src/main/java/com/wofeng/articlemanagement/mvcConfig/WebMvcConfig.java
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer{
 
 
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //registry.addViewController("/login").setViewName("login");
